3b77224738319a71040fd67937b5b29f51b3446a
rochellev <rochellevroberts@utexas.edu>
2019-08-20 16:08:00 -0700

    update program to use rest sharp

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Threading.Tasks;
 
 namespace ConsoleApiCall
 {
@@ -6,7 +9,29 @@ namespace ConsoleApiCall
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1
+            var client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+            // 2
+            var request = new RestRequest("home.json?api-key=[YOUR-API-KEY-HERE]", Method.GET);
+            // 3
+            var response = new RestResponse();
+
+            // 4
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            Console.WriteLine(response.Content);
+        }
+
+        // 5
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
         }
     }
 }
